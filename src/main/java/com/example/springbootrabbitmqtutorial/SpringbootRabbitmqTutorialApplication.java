fc35e4f38314b0c35538be94d9b2e65a6e9ea5a5
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2023-05-18 09:21:32 +0530

    init project

---
+++ src/main/java/com/example/springbootrabbitmqtutorial/SpringbootRabbitmqTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrabbitmqtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqTutorialApplication.class, args);
+	}
+
+}
